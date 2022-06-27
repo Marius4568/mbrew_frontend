3a4b691beb1742308ba2b5a3fa1739274e76b61d
Marius Urbonas <59611229+Marius4568@users.noreply.github.com>
2022-06-27 15:11:16 +0300

    deleted unused vars

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import Products from '../components/Products/Products';
 import Section from '../components/Section/Section';
 
-import { useGlobalShopContext } from '../lib/shopContext';
-
 const Home = () => {
-  // const { qty } = useGlobalShopContext();
-
   return (
     <>
       <Section title="Selection">
